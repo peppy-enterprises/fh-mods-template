569ba35ba3714f950c900a0e9dad2cc9c9ebdb64
Fran Kelava <fran.kelava@outlook.com>
2025-05-27 03:01:05 +0200

    Initial commit.

---
+++ module.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+using Fahrenheit.Core;
+
+namespace Fahrenheit.Modules.Template;
+
+[FhLoad(FhGameType.FFX)]
+public class TemplateModule : FhModule {
+    public TemplateModule() { }
+
+    public override bool init(FileStream global_state_file) { }
+
+    public override void load_local_state(FileStream? local_state_file, FhLocalStateInfo local_state_info) { }
+    public override void save_local_state(FileStream  local_state_file)                                    { }
+}
